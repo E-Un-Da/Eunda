fa0b7a485e012da553a4b480f9d7916551a53b4b
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-22 17:36:05 +0900

    fix: 리포지토리 타입 변경

--- src/main/java/com/hanghae/eunda/repository/RecordRepository.java
+++ src/main/java/com/hanghae/eunda/repository/RecordRepository.java
@@ -3,5 +3,5 @@ package com.hanghae.eunda.repository;
 import com.hanghae.eunda.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecordRepository extends JpaRepository<Card, Long> {
+public interface RecordRepository extends JpaRepository<Record, Long> {
 }
