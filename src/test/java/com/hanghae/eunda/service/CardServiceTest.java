866d631725384db197165ff6e07959fb92515add
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-24 21:27:42 +0900

    test: 테스트 코드 작성 중

--- src/test/java/com/hanghae/eunda/service/CardServiceTest.java
+++ src/test/java/com/hanghae/eunda/service/CardServiceTest.java
@@ -1,8 +1,9 @@
 package com.hanghae.eunda.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.hanghae.eunda.dto.card.CardStatusRequestDto;
+import com.hanghae.eunda.entity.Member;
+import com.hanghae.eunda.repository.CardRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -11,15 +12,29 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
 class CardServiceTest {
 
+    @Mock
+    private CardRepository cardRepository;
+
     @Autowired
     private CardService cardService;
+
+    @Mock
+    private RedissonClient redissonClient;
 
     @Test
     public void testChangeCardStatusConcurrency() throws InterruptedException {
@@ -51,4 +66,35 @@ class CardServiceTest {
         executor.shutdown();
         executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
     }
+
+
+    @Test
+    public void testChangeCardStatusWithLockAcquired() throws InterruptedException {
+        // RedissonClient로부터 RLock을 생성하여 mock으로 대체합니다.
+        RLock mockLock = mock(RLock.class);
+
+        // redissonClient.getLock() 메서드를 호출할 때 mockLock을 반환하도록 설정합니다.
+        when(redissonClient.getLock(anyString())).thenReturn(mockLock);
+
+        // mockLock.tryLock() 메서드를 호출했을 때 true를 반환하도록 설정합니다.
+        when(mockLock.tryLock(anyLong(), anyLong(), any())).thenReturn(true);
+
+        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+
+        Member fakeMember = new Member();
+        // 가짜 Member 객체에 ID 값 설정
+        fakeMember.setId(123L);
+
+        // 테스트할 메서드를 호출합니다.
+        // 여기서는 예시로 ID와 CardStatusRequestDto를 전달하고 있습니다.
+        String result = cardService.changeCardStatus(2L, new CardStatusRequestDto(), mockRequest);
+
+        // 여기서는 어떤 결과를 예측하는지 확인합니다.
+        // 이 예시에서는 "상태 변경 완료"가 반환되어야 한다고 가정하였습니다.
+        assertEquals("상태 변경 완료", result);
+
+        // lock.unlock() 메서드가 1번 호출되었는지 확인합니다.
+        verify(mockLock, times(1)).unlock();
+    }
+
 }
