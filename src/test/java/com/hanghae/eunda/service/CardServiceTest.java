02b36c0944452593d302c40f58e11c5e3818ad38
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-24 22:36:30 +0900

    test: 동시성제어 테스트코드 작성

--- src/test/java/com/hanghae/eunda/service/CardServiceTest.java
+++ src/test/java/com/hanghae/eunda/service/CardServiceTest.java
@@ -6,11 +6,13 @@ import com.hanghae.eunda.repository.CardRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.redisson.api.RLock;
@@ -67,34 +69,39 @@ class CardServiceTest {
         executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
     }
 
-
     @Test
-    public void testChangeCardStatusWithLockAcquired() throws InterruptedException {
-        // RedissonClient로부터 RLock을 생성하여 mock으로 대체합니다.
-        RLock mockLock = mock(RLock.class);
+    public void testConcurrentCardStatusChange() throws InterruptedException {
+        int threadCount = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
 
-        // redissonClient.getLock() 메서드를 호출할 때 mockLock을 반환하도록 설정합니다.
-        when(redissonClient.getLock(anyString())).thenReturn(mockLock);
-
-        // mockLock.tryLock() 메서드를 호출했을 때 true를 반환하도록 설정합니다.
-        when(mockLock.tryLock(anyLong(), anyLong(), any())).thenReturn(true);
-
-        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+        Long cardId = 1L;
+        CardStatusRequestDto cardStatusRequestDto = new CardStatusRequestDto();
+        cardStatusRequestDto.setStatus("TODO");
 
         Member fakeMember = new Member();
-        // 가짜 Member 객체에 ID 값 설정
-        fakeMember.setId(123L);
+        fakeMember.setId(1L);
+        fakeMember.setEmail("test@example.com");
 
-        // 테스트할 메서드를 호출합니다.
-        // 여기서는 예시로 ID와 CardStatusRequestDto를 전달하고 있습니다.
-        String result = cardService.changeCardStatus(2L, new CardStatusRequestDto(), mockRequest);
 
-        // 여기서는 어떤 결과를 예측하는지 확인합니다.
-        // 이 예시에서는 "상태 변경 완료"가 반환되어야 한다고 가정하였습니다.
-        assertEquals("상태 변경 완료", result);
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+        mockRequest.setAttribute("member", fakeMember);
 
-        // lock.unlock() 메서드가 1번 호출되었는지 확인합니다.
-        verify(mockLock, times(1)).unlock();
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+
+                    String result = cardService.changeCardStatus(cardId, cardStatusRequestDto, mockRequest);
+
+                    Assertions.assertEquals("상태 변경 완료", result);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await(); // 모든 스레드가 실행 완료될 때까지 대기
+        executorService.shutdown();
     }
+
 
 }
