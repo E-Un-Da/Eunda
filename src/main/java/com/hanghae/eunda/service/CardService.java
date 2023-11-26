f9838b7310e54887a40652056acb156c2d3ef2e5
hongui <honge7694@naver.com>
2023-11-27 02:46:49 +0900

    redis lettuce를 이용한 동시성 제어

--- src/main/java/com/hanghae/eunda/service/CardService.java
+++ src/main/java/com/hanghae/eunda/service/CardService.java
@@ -5,7 +5,9 @@ import com.hanghae.eunda.dto.card.CardResponseDto;
 import com.hanghae.eunda.dto.card.CardStatusRequestDto;
 import com.hanghae.eunda.entity.*;
 import com.hanghae.eunda.repository.CardRepository;
+import com.hanghae.eunda.repository.RedisLockRepository;
 import com.hanghae.eunda.repository.StudyRepository;
+import jakarta.persistence.LockModeType;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +21,9 @@ public class CardService {
 
     private final CardRepository cardRepository;
     private final StudyRepository studyRepository;
+
+    // redis repository
+    private final RedisLockRepository redisLockRepository;
 
     @Transactional
     public void createCard(Long studyId, CardRequestDto requestDto, HttpServletRequest req) {
@@ -61,19 +66,62 @@ public class CardService {
         return "카드 삭제 성공";
     }
 
+//    @Transactional
+//    public String changeCardStatus(Long id, CardStatusRequestDto cardStatusRequestDto, HttpServletRequest req) {
+//        Member member = (Member) req.getAttribute("member");
+//        Card card = findCard(id);
+//        if (!member.getId().equals(card.getMember().getId())) {
+//            throw new IllegalArgumentException("너 스터디 멤버 아니잖아");
+//        }
+//        StatusEnumType newStatus = StatusEnumType.valueOf(cardStatusRequestDto.getStatus());
+//        card.changeCardStatus(newStatus);
+//
+//
+//        return "상태 변경 완료";
+//    }
+
+    static int count = 100; // test용
     @Transactional
     public String changeCardStatus(Long id, CardStatusRequestDto cardStatusRequestDto, HttpServletRequest req) {
-        Member member = (Member) req.getAttribute("member");
-        Card card = findCard(id);
-        if (!member.getId().equals(card.getMember().getId())) {
-            throw new IllegalArgumentException("너 스터디 멤버 아니잖아");
+        log.info("changeCardStatus 실행됨 id : {}, cardStatusRequestDto : {}, HttpServletRequest : {}", id, cardStatusRequestDto, req);
+        while (!redisLockRepository.lock(id)) { // lock 획득 시도 실패시
+            log.info("lock 획득 시도 중...");
+            try {
+                Thread.sleep(100); // 0.1초 마다 다시 획득 시도
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-        StatusEnumType newStatus = StatusEnumType.valueOf(cardStatusRequestDto.getStatus());
-        card.changeCardStatus(newStatus);
 
+        // test용
+        count -= 1;
+
+        // test용
+        if (count % 2 == 0) {
+            throw new IllegalArgumentException("동시성 제어?");
+        }
+
+        // lock 획득 성공시
+        try {
+            log.info("lock 획득 성공");
+
+            Member member = (Member) req.getAttribute("member");
+            Card card = findCard(id);
+
+            if (!member.getId().equals(card.getMember().getId())) {
+                throw new IllegalArgumentException("너 스터디 멤버 아니잖아");
+            }
+
+            StatusEnumType newStatus = StatusEnumType.valueOf(cardStatusRequestDto.getStatus());
+            card.changeCardStatus(newStatus);
+        } finally {
+            log.info("lock 반납");
+            redisLockRepository.unlock(id); // lock 해제
+        }
 
         return "상태 변경 완료";
     }
+
     private Card findCard(Long id) {
         return cardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("선택한 카드가 없습니다"));
     }
