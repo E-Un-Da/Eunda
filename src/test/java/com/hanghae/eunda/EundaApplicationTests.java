f20b009145a58b7f17e534086571e18d378696e8
munee <121735319+alaneelee@users.noreply.github.com>
2023-11-28 20:21:46 +0900

    temp: 테스트코드 주석처리 한번더!

--- src/test/java/com/hanghae/eunda/EundaApplicationTests.java
+++ src/test/java/com/hanghae/eunda/EundaApplicationTests.java
@@ -3,11 +3,6 @@ package com.hanghae.eunda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EundaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
